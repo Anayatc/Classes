27ff5b12913894830baa5543e334098eef832739
Anayatc <Sincity9490>
2017-04-27 08:25:56 +0100

    basic object orientated design and analysis

--- ex43.py
+++ ex43.py
@@ -57,6 +57,12 @@ class CentralCorridor(Scene):
             print("His clown costume is flowing and moving around his body, which throws")
             print("off your aim. Your later hits his costume but misses him entirely. This")
             print("complete ruins his brand new costume his mother bought him, which")
+            print("makes him fly into a rage and blast you repeatedly in the face until")
+            print("you are dead. Then he eats you.")
+            return 'death'
+
+        elif action == "dodge!":
+
 
 
 
