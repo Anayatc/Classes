2d65de964eae67326f172ea1be051621fa6df421
anayatc@gmail.com <anayatc@sky.com>
2017-04-29 21:04:32 +0100

    composition

--- ex44.1.py
+++ ex44.1.py
@@ -1,10 +1,27 @@
 class Other(object):
 
     def override(self):
-        print "OTHER override()"
+        print("OTHER override()")
 
     def implicit(self):
-        print "OTHER implicit()"
+        print("OTHER implicit()")
 
     def altered(self):
-        print "OTHER altered()"
+        print("OTHER altered()")
+
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
