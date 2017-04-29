53b12bcfaea07d8d69cdee94e51a681040b8c4e6
Anayatc <anayatc@sky.com>
2017-04-29 20:44:19 +0100

    inheritance vs. composition

--- ex44.py
+++ ex44.py
@@ -1,11 +1,23 @@
 class Parent(object):
 
+    def override(self):
+        print("PARENT override()")
+
     def implicit(self):
         print("PARENT implicit()")
 
+    def altered(self):
+        print("PARENT altered()")
 
 class Child(Parent):
-    pass
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered()
+        print("CHILD, AFTER PARENT altered()")
 
 dad = Parent()
 son = Child()
@@ -13,40 +25,9 @@ son = Child()
 dad.implicit()
 son.implicit()
 
+dad.override()
+son.override()
 
-class Parent1(object):
-
-    def override(self):
-        print("PARENT override()")
-
-
-class Child1(Parent):
-
-    def override(self):
-        print("CHILD override()")
-
-dad1 = Parent1()
-son1 = Child1()
-
-dad1.override()
-son1.override()
-
-
-class Parent2(object):
-
-    def altered(self):
-        print("PARENT altered()")
-
-class Child2(Parent):
-
-    def altered(self):
-        print("CHILD, BEFORE PARENT altered()")
-        super(Child, self).altered()
-        print("CHILD, AFTER PARENT altered()")
-
-dad2 = Parent2()
-son2 = Child2()
-
-dad2.altered()
-son2.altered()
+dad.altered()
+son.altered()
 
